13cbda24cf7ad53932c4324677c9d6249521eced
Edwin <edwin.barrera1025@gmail.com>
2026-05-05 17:19:41 +0200

    Version with revision

--- nivel1/Exercici1/NumberCheckerTest.php
+++ nivel1/Exercici1/NumberCheckerTest.php
@@ -7,29 +7,110 @@ use PHPUnit\Framework\TestCase;
 
 final class NumberCheckerTest extends TestCase
 {
-    public function numProvider(): array
+    public function par(): array
     {
-        $int = [];
-        for ($i = 0; $i < 10; $i++) {
-            $int['caso' . $i] = [mt_rand(-100, 100)];
-        }
+        $int = [
+            'par 6' => [6]
+        ];
+        return $int;
+    }
+    public function impar(): array
+    {
+        $int = [
+            'impar 9' => [9]
+        ];
+        return $int;
+    }
+    public function negativo(): array
+    {
+        $int = [
+            'negativo -4' => [-4]
+        ];
+        return $int;
+    }
+    public function positivo(): array
+    {
+        $int = [
+            'positivo 1' => [1]
+        ];
+        return $int;
+    }
+    public function cero(): array
+    {
+        $int = [
+            'cero' => [0]
+        ];
         return $int;
     }
 
-    #[DataProvider('numProvider')]
-    public static function testIsEven(int $num): void
+    public function string(): array
+    {
+        $int = [
+            'string' => ['text']
+        ];
+        return $int;
+    }
+
+    #[DataProvider('par')]
+    public static function testIsEvenEven(int $num): void
     {
         $numCheck = new NumberChecker($num);
         $testBool = $numCheck->isEven();
-        $cond = $num & 0;
-        self::assertSame($cond, $testBool);
+        self::assertSame(true, $testBool);
     }
 
+    #[DataProvider('impar')]
+    public static function testIsEvenOdd(int $num): void
+    {
+        $numCheck = new NumberChecker($num);
+        $testBool = $numCheck->isEven();
+        self::assertSame(false, $testBool);
+    }
+
+    #[DataProvider('cero')]
+    public static function testIsEvenZeroException(int $num): void
+    {
+        $numCheck = new NumberChecker($num);
+        $testBool = $numCheck->isEven();
+        self::assertSame(true, $testBool);
+    }
+
+    #[DataProvider('string')]
+    public function testIsEvenStringException(mixed $num): void
+    {
+        $this->expectException(TypeError::class);
+        $numCheck = new NumberChecker($num);
+        $numCheck->isEven();
+    }
+
+    #[DataProvider('positivo')]
     public static function testIsPositive(int $num): void
     {
         $numCheck = new NumberChecker($num);
         $testBool = $numCheck->isPositive();
-        $cond = $num > 0;
-        self::assertSame($cond, $testBool);
+        self::assertSame(true, $testBool);
+    }
+
+    #[DataProvider('negativo')]
+    public static function testIsNegative(int $num): void
+    {
+        $numCheck = new NumberChecker($num);
+        $testBool = $numCheck->isPositive();
+        self::assertSame(false, $testBool);
+    }
+    #[DataProvider('cero')]
+    public static function testIsPositiveZeroException(int $num): void
+    {
+        $numCheck = new NumberChecker($num);
+        $testBool = $numCheck->isPositive();
+        self::assertSame(false, $testBool);
+    }
+
+    #[DataProvider('string')]
+    public function testIsPositiveStringException(mixed $num): void
+    {
+        $this->expectException(TypeError::class);
+        $numCheck = new NumberChecker($num);
+        $numCheck->isPositive();
     }
 }
